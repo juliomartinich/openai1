deffdef3961738bb6f3d8e98104fc25ac90e4bbf
Julio Martinich <julio.martinich@gmail.com>
2023-06-06 23:26:56 -0400

    AEDE: Asistente Extractor De Entidades

--- aede_ail.py
+++ aede_ail.py
@@ -1,29 +1,45 @@
 from flask import Flask, render_template, request, redirect, session, jsonify
 import os, openai, logging, datetime, traceback
 import web_funciones as wf
+import json
 
 def inicia_chat():
    chat = []  # Reinicia la variable de conversación
-   mensaje = "Eres AIL, el Asistente de Información Legal de Consorcio y yo soy un Cajero. "
-   mensaje += "Responde basado solo en la informacion que yo te entregue. "
-   mensaje += "Si la pregunta no se refiere a esa informacion responde No tengo informacion."
-   chat.append({"role": "user", "content": mensaje})
+
+   prompt = """
+Eres un asistente de una aerolinea, tu misión es obtener ciudad de origen y destino, y fechas de ida y vuelta, preguntando al cliente.  Cada vez que obtengas un dato debes entregar una respuesta en formato JSON al finalizar la respuesta normal.  Hoy es martes 6 de Junio de 2023, si te piden una fecha en forma relativa dedúcela a partir de hoy usando el calendario.
+---
+ejemplo 1: Quiero viajar a Santiago
+
+Entiendo que quiere viajar a Santiago, ¿desde que ciudad de origen?
+
+JSON {"origen": "Valdivia", "destino":"Santiago", "fecha ida":"", "fecha vuelta":""}
+---
+
+ejemplo 2: Quiero viajar el primer martes de septiembre
+
+de acuerdo, como el 1ro de septiembre cae viernes, sería el 5 de septiembre, ¿desde donde desea viajar?
+
+JSON {"origen": "", "destino":"", "fecha ida":"05-09-2023", "fecha vuelta":""}
+---
+
+   """
+   chat.append({"role": "user", "content": prompt})
+
+   logging.info("Prompt: " + prompt)
+
    return chat
 
 app = Flask(__name__)
 openai.api_key = os.environ.get('OPENAI_API_KEY')
 app.secret_key = openai.api_key  # API openai de clave secreta para la sesión
 
-(parrafos, archivos, textos, embeddings) = wf.read_stored_embeddings('mis_word_embed.csv')
+#configura logging
+logging.basicConfig(filename='aede.log', level=logging.INFO)
 
 # Variable global para mantener el contexto de la conversación
 conversacion = inicia_chat()
-mensaje = "aca te entrego informacion: " + textos[0]
-conversacion.append({"role": "user", "content": mensaje})
-primero = True
 
-#configura logging
-logging.basicConfig(filename='ail.log', level=logging.INFO)
 
 #-------------------------@
 @app.errorhandler(404)
@@ -60,7 +76,7 @@ def log_request_info():
 def chatbot():
     if 'username' not in session: return redirect('/login')
 
-    return render_template('chatbot.html')
+    return render_template('aede.html')
 
 #+++++++++++++++++++++++++++++++++++++++
 @app.route('/login', methods=['GET', 'POST'])
@@ -96,47 +112,39 @@ def logout():
 @app.route('/reset', methods=['POST'])
 def reset_conversacion():
     if 'username' not in session: return redirect('/login')
-    global conversacion, primero
+    global conversacion
 
     conversacion = inicia_chat()
-    primero = True
     return redirect('/')
 
 #-------------------------@
 # Ruta API REST para recibir y procesar las solicitudes del usuario
-@app.route('/mensaje', methods=['POST'])
+@app.route('/chat', methods=['POST'])
 def procesar_mensaje():
     if 'username' not in session: return redirect('/login')
-    global contexto, imc, pc_mas_cercano
+    global contexto
 
-    mensaje_usuario = request.form['mensaje']
+    mensaje_usuario = request.json['user_input'] + ". Recuerda deducir fecha y el JSON al final."
 
     logging.info("Usuario: "+ session["username"]+ "/ Pregunta: "+ mensaje_usuario)
 
-    respuesta_chatbot = obtener_respuesta_chatbot(mensaje_usuario)
-    respuesta = { 'mensaje': respuesta_chatbot, 'pc_mas_cercano': pc_mas_cercano }
-    respuesta['contexto'] = contexto
-    respuesta['imc'] = imc
-    # ahora devuelvo un json
-    return respuesta
+    ( respuesta_chatbot, entities ) = obtener_respuesta_chatbot(mensaje_usuario)
+
+    logging.info("Usuario: "+ session["username"]+ "/ Respuesta: "+ respuesta_chatbot)
+
+    respuesta_sin_JSON = cortar_string(respuesta_chatbot,"JSON")
+
+    response = { 'message': respuesta_sin_JSON, 'entities': entities }
+    return jsonify(response)
 
 #----------------------------------------------------------------
 # Función para obtener la respuesta del chatbot utilizando OpenAI
 # esta no es una ruta, sino una funcion 
 def obtener_respuesta_chatbot(mensaje_usuario):
     global conversacion
-    global parrafos, archivos, textos, embeddings
-    global primero, contexto, imc, pc_mas_cercano
 
-    if primero:
-        pregunta = "¿ " + mensaje_usuario + " ?"
-        vector = wf.read_embedding(pregunta)
-        (contexto, imc, pc_mas_cercano) = wf.busca_contexto(vector, embeddings, textos)
-        conversacion.append({"role": "user", "content": contexto})
-        primero = False
+    mensaje = mensaje_usuario
 
-    mensaje = "responde la pregunta solo con la informacion entregada en no mas de 100 palabras: ¿ "
-    mensaje += wf.eliminar_signos_especiales(mensaje_usuario) + " ? "
     conversacion.append({"role": "user", "content": mensaje})
 
     # Obtener la respuesta del chatbot y actualizar el contexto de la conversación
@@ -156,12 +164,51 @@ def obtener_respuesta_chatbot(mensaje_usuario):
     except Exception as e:
         return "Error: " + e.__str__()
 
-    wf.escribir_en_archivo_csv([], mensaje_usuario, respuesta_chatbot, imc, pc_mas_cercano, "ail_preguntas.csv")
+    #wf.escribir_en_archivo_csv([], mensaje_usuario, respuesta_chatbot, imc, pc_mas_cercano, "ail_preguntas.csv")
 
     conversacion.append({"role": "assistant", "content": respuesta_chatbot})
 
+    entities = detect_entities(respuesta_chatbot)
+
     # Retornar la respuesta del chatbot al usuario
-    return respuesta_chatbot
+    return respuesta_chatbot, entities
+
+def detect_entities(respuesta_IA):
+    # Aquí debes implementar la lógica para detectar las entidades
+    # y generar el diccionario de código-valor
+    # Puedes utilizar bibliotecas de procesamiento de lenguaje natural 
+    # como Spacy o NLTK
+
+    # Ejemplo de entidades detectadas
+    
+    entities = encontrar_json(respuesta_IA)
+
+    return entities
+
+def encontrar_json(cadena):
+    # Buscar el inicio y fin del JSON dentro de la cadena
+    inicio = cadena.find('{')
+    fin = cadena.find('}', inicio) + 1
+    
+    if inicio == -1 or fin == 0:
+        # No se encontró un JSON válido
+        return None
+    
+    json_str = cadena[inicio:fin]
+    logging.info("json str: " + json_str )
+    
+    try:
+        # Analizar el JSON y devolver el objeto resultante como un diccionario
+        json_dict = json.loads(json_str)
+        logging.info(json_dict )
+        return json_dict
+    except json.JSONDecodeError:
+        # El JSON no es válido
+        return None
+
+def cortar_string(string, palabra):
+    partes = string.split(palabra, 1)  # Dividir solo en la primera ocurrencia
+    return partes[0]
 
 if __name__ == '__main__':
     # Configurar el controlador de archivo
